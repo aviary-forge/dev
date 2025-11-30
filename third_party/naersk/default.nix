101665350a96cb138682fae6feac8151ddcae981
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2025-11-30 10:50:22 -0800

    fix naersk so we always use fenix toolchain

--- third_party/naersk/default.nix
+++ third_party/naersk/default.nix
@@ -1,3 +1,11 @@
 { dev, pkgs, ... }:
 
-pkgs.callPackage dev.third_party.nix.naersk { }
+let
+  naersk' = pkgs.callPackage dev.third_party.nix.naersk { };
+  toolchain = pkgs.fenix.complete.toolchain;
+
+in
+naersk'.override {
+  cargo = toolchain;
+  rustc = toolchain;
+}
