5a4f0f6e80151a64187fbb3ff598b5142d5588a5
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2025-12-13 09:12:42 -0800

    add ocaml to neovim

---
+++ users/denbeigh/neovim/modules/languages/ocaml.nix
@@ -0,0 +1,18 @@
+{ lib, pkgs, ... }:
+let
+  inherit (lib) mkDefault;
+in
+{
+  config = {
+    plugins = {
+      lsp.servers.ocamllsp = {
+        enable = mkDefault true;
+        package = mkDefault pkgs.ocamlPackages.ocaml-lsp;
+      };
+
+      rooter.patterns = [ "dune-project" ];
+      treesitter.settings.ensure_installed = [ "ocaml" ];
+
+    };
+  };
+}
