90191770df6ede48e6182bbd7d1e6f3762f38bba
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2025-12-13 18:10:32 -0800

    nix-darwin initialisation and builder system config

---
+++ nix/darwin.nix
@@ -0,0 +1,45 @@
+{ dev, pkgs, ... }:
+
+let
+  activateSystem = (system:
+    pkgs.writeShellApplication {
+      name = "activate-system";
+
+      text = ''
+        if [[ "$EUID" -ne "0" ]]
+        then
+          echo "system must be activated as root" >&2
+          exit 1
+        fi
+
+        nix-env -p /nix/var/nix/profiles/system --set ${system}
+        ${system}/sw/bin/darwin-rebuild activate
+      '';
+    });
+
+in
+rec {
+  baseModule = { ... }: {
+    nixpkgs.pkgs = dev.third_party.nixpkgs;
+
+    # allow us to skip the global configuration check, similar to building with
+    # flakes
+    system.checks.verifyNixPath = false;
+  };
+
+  eval = configuration:
+    let
+      config = (dev.third_party.darwin.eval {
+        configuration = { ... }: {
+          imports = [ baseModule configuration ];
+        };
+
+      });
+    in
+    {
+      inherit (config) system;
+      activate = (activateSystem config.system);
+    };
+
+  activate = activateSystem;
+}
