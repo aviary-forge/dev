b5a3aed7d315129fb0f2e19f7627b1b1f05b41ae
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2025-12-13 19:34:55 -0800

    config tweaks for macos builder

--- systems/darwin/builder.nix
+++ systems/darwin/builder.nix
@@ -1,18 +1,38 @@
 { dev, ... }:
 
-dev.nix.darwin.eval {
-  networking = {
-    hostName = "builder";
-    domain = "sfo.denbeigh.cloud";
-  };
+dev.nix.darwin.eval (
+  { pkgs, ... }:
+  {
+    networking =
+      {
+        hostName = "builder";
+        domain = "sfo.denbeigh.cloud";
+      };
 
-  services.openssh.enable = true;
+    environment.systemPackages = [ pkgs.stdenv pkgs.stdenv.cc pkgs.git ];
+    services.openssh.enable = true;
 
-  users.users.denbeigh = {
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBrWuq0cLFKo4KKLYKF/SG3U/6/7U0o7JDHDeJOwadAf"
-    ];
-  };
+    system.defaults = {
+      loginwindow.autoLoginUser = "denbeigh";
+      NSGlobalDomain.NSDocumentSaveNewDocumentsToCloud = false;
+      WindowManager.EnableStandardClickToShowDesktop = false;
+      SoftwareUpdate.AutomaticallyInstallMacOSUpdates = false;
+      dock = {
+        persistent-apps = [ ];
+        persistent-others = [ ];
+      };
+      universalaccess.reduceTransparency = true;
+    };
 
-  system.stateVersion = 6;
-}
+    time.timeZone = "GMT";
+    users.users.denbeigh = {
+      packages = [ dev.users.denbeigh.neovim ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBrWuq0cLFKo4KKLYKF/SG3U/6/7U0o7JDHDeJOwadAf"
+      ];
+    };
+
+    system.stateVersion = 6;
+    system.primaryUser = "denbeigh";
+  }
+)
