7df42920d01029e95b4211cee1c751953ad452a2
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2025-12-14 12:59:25 -0800

    initial nix-instantiate-driven evaluation

---
+++ heap/evaluate.nix
@@ -0,0 +1,40 @@
+{
+  devPath ? ../.,
+  systems ? [ builtins.currentSystem ],
+}:
+
+let
+  inherit (builtins) unsafeDiscardStringContext;
+
+  collateTargetMeta =
+    mkLabel: target:
+    let
+      treePath = unsafeDiscardStringContext (mkLabel target);
+    in
+    {
+      name = treePath;
+      value = {
+        inherit treePath;
+        system = unsafeDiscardStringContext target.system;
+        drvPath = unsafeDiscardStringContext target.drvPath;
+        outputPath = unsafeDiscardStringContext target.outPath;
+      };
+    };
+
+  evaluateSystem =
+    system:
+    let
+      localDev = import devPath { localSystem = system; };
+      inherit (localDev.nix.readTree) mkLabel;
+      # TODO: do we want to invoke readTree.gather here, instead of in root?
+      inherit (localDev.ci) targets;
+    in
+    {
+      name = system;
+      value = builtins.listToAttrs (builtins.map (collateTargetMeta mkLabel) targets);
+    };
+
+  evaluateAllSystems = systems: builtins.listToAttrs (builtins.map evaluateSystem systems);
+in
+
+evaluateAllSystems systems
