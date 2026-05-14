fc0364c79fbbee4ae775e7d3b63a5a876481999f
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2026-05-14 15:01:30 -0700

    users denbeigh: add missing erlang file

---
+++ users/denbeigh/neovim/modules/languages/erlang.nix
@@ -0,0 +1,27 @@
+{ lib, pkgs, ... }:
+
+let
+  inherit (lib) mkDefault;
+in
+{
+
+  config = {
+    plugins = {
+      lsp.servers.elp = {
+        enable = mkDefault true;
+        cmd = [
+          "${pkgs.erlang-language-platform}/bin/elp"
+          "server"
+        ];
+      };
+
+      treesitter.settings.ensure_installed = [ "erlang" ];
+      rooter.patterns = [
+        "rebar.config"
+        "rebar.lock"
+      ];
+    };
+
+    extraPackages = with pkgs; [ beam28Packages.erlang stdenv.cc ];
+  };
+}
