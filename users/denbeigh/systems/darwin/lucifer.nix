2f8f0efb9c97d45a8b4976d7ccc7ea9367444636
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2026-05-14 18:48:16 -0700

    users denbeigh lucifer: add darwin/opencode

--- users/denbeigh/systems/darwin/lucifer.nix
+++ users/denbeigh/systems/darwin/lucifer.nix
@@ -1,24 +1,32 @@
 { dev, ... }:
 
-dev.nix.darwin.eval {
-  imports = [
-    ../../modules/nix-darwin/standard.nix
-  ];
-  config = {
-    dev.denbeigh = {
-      machine = {
-        hostname = "lucifer";
-        location = dev.users.denbeigh.utils.locations.locations.sf;
+dev.nix.darwin.eval (
+  { pkgs, ... }:
+  {
+    imports = [
+      ../../modules/nix-darwin/standard.nix
+    ];
+    config = {
+      dev.denbeigh = {
+        machine = {
+          hostname = "lucifer";
+          location = dev.users.denbeigh.utils.locations.locations.sf;
+        };
+        user = {
+          username = "denbeigh";
+          keys = [ "id_ed25519" ];
+        };
       };
-      user = {
-        username = "denbeigh";
-        keys = [ "id_ed25519" ];
-      };
+
+      system.primaryUser = "denbeigh";
+      system.stateVersion = 5;
+
+      environment.systemPackages = with pkgs; [
+        ollama
+        opencode
+      ];
+
+      ids.gids.nixbld = 30000;
     };
-
-    system.primaryUser = "denbeigh";
-    system.stateVersion = 5;
-
-    ids.gids.nixbld = 30000;
-  };
-}
+  }
+)
