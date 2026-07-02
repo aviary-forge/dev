de59553da6e25cdf5e9d97a847ec54052d9dfeda
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2026-07-02 15:03:50 -0700

    add ruff/ty config

--- users/denbeigh/email-classifier/shell.nix
+++ users/denbeigh/email-classifier/shell.nix
@@ -6,8 +6,11 @@ let
   pkgs = dev.third_party.nixpkgs;
 in
 pkgs.mkShell {
-  packages = with pkgs.python314Packages; [
-    uv
-    python
-  ];
+  packages =
+    (with pkgs.python314Packages; [
+      uv
+      python
+      ruff
+    ])
+    ++ [ pkgs.ty ];
 }
