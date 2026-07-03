01d8dcd90b794a1091970d2ffb1895d26aaedb9a
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2026-07-03 16:37:36 -0700

    fix addl packages

--- users/denbeigh/email-classifier/shell.nix
+++ users/denbeigh/email-classifier/shell.nix
@@ -6,11 +6,32 @@ let
   pkgs = dev.third_party.nixpkgs;
 in
 pkgs.mkShell {
+
   packages =
     (with pkgs.python314Packages; [
       uv
       python
       ruff
     ])
-    ++ [ pkgs.ty ];
+    ++ (with pkgs; [
+      ty
+      stdenv.cc.cc.lib
+      zlib
+      git-xet
+      git-lfs
+      git-lfs-transfer
+    ]);
+
+  shellHook =
+    let
+      libPath =
+        with pkgs;
+        lib.makeLibraryPath [
+          stdenv.cc.cc.lib
+          zlib
+        ];
+    in
+    ''
+      export LD_LIBRARY_PATH="${libPath}:/run/opengl-driver/lib:$LD_LIBRARY_PATH"
+    '';
 }
