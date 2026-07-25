b54cdc02f575767f83a128a738a9ccbd93eda2e8
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2026-07-25 12:27:26 -0700

    fix nixpkgs: move permittedInsecurePackages out of ahoy module
    nixpkgs.config can't be mutated from a NixOS module when nixpkgs is
    pre-instantiated externally. Move the *arr dotnet 6 allowlist into
    third_party/nixpkgs/default.nix where it belongs.

--- third_party/nixpkgs/default.nix
+++ third_party/nixpkgs/default.nix
@@ -35,6 +35,14 @@ let
       # Forbids our meta.ci attribute
       # https://github.com/NixOS/nixpkgs/pull/191171#issuecomment-1260650771
       checkMeta = false;
+      # *arr stack still depends on insecure .NET 6 packages
+      # https://github.com/NixOS/nixpkgs/issues/360592
+      permittedInsecurePackages = [
+        "aspnetcore-runtime-6.0.36"
+        "aspnetcore-runtime-wrapped-6.0.36"
+        "dotnet-sdk-6.0.428"
+        "dotnet-sdk-wrapped-6.0.428"
+      ];
     };
 
     inherit localSystem crossSystem;
