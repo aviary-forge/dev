35c2865928a44e75871114728c1f969e607ea5b1
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2026-09-06 12:45:56 -0700

    secrets: rename cfdyndnsApiToken to cloudflareApiToken

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -17,7 +17,7 @@ in
   "buildkite-ssh-private-key.age" = key [ systems.aviary ];
   "tailscaleAuthKey.age" = key [ systems.aviary ];
   "digitalOceanAPIKey.age" = key [ systems.aviary ];
-  # "cfdyndnsApiToken.age" = key [ systems.aviary ];  # disabled: key rotation
+  "cloudflareApiToken.age" = key [ systems.aviary ];
   "terraform.age" = key [ systems.aviary ];
   "fontDeployKey.age" = key [ systems.aviary ];
 }
