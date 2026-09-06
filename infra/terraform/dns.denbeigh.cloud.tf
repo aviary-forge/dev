dc0c3920c233e5ac0b1683dc85991102ff9c12ef
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2026-09-06 12:39:58 -0700

    terraform: port denbeigh.cloud records to cloudflare

--- infra/terraform/dns.denbeigh.cloud.tf
+++ infra/terraform/dns.denbeigh.cloud.tf
@@ -6,50 +6,39 @@ data "tailscale_devices" "bruce" {
   name_prefix = "bruce"
 }
 
-data "digitalocean_domain" "denbeigh_cloud" {
-  name = "denbeigh.cloud"
+data "cloudflare_zone" "denbeigh_cloud" {
+  filter = {
+    name = "denbeigh.cloud"
+  }
 }
 
-resource "digitalocean_record" "denbeigh_cloud_ns_1" {
-  domain = data.digitalocean_domain.denbeigh_cloud.id
-  type   = "NS"
-  name   = "@"
-  value  = "ns1.digitalocean.com."
+# All records on this zone must stay proxied = false (grey cloud):
+# CF proxy breaks direct SSH and the nix binary cache's origin model.
+resource "cloudflare_dns_record" "bruce_denbeigh_cloud" {
+  zone_id = data.cloudflare_zone.denbeigh_cloud.id
+  name    = "bruce"
+  content = "23.145.80.211"
+  type    = "A"
+  ttl     = 3600
+  proxied = false
 }
 
-resource "digitalocean_record" "denbeigh_cloud_ns_2" {
-  domain = data.digitalocean_domain.denbeigh_cloud.id
-  type   = "NS"
-  name   = "@"
-  value  = "ns2.digitalocean.com."
+resource "cloudflare_dns_record" "bruce_tailscale_denbeigh_cloud" {
+  zone_id = data.cloudflare_zone.denbeigh_cloud.id
+  name    = "bruce.tailscale"
+  content = data.tailscale_devices.bruce.devices[0].addresses[0]
+  type    = "A"
+  ttl     = 3600
+  proxied = false
 }
 
-resource "digitalocean_record" "denbeigh_cloud_ns_3" {
-  domain = data.digitalocean_domain.denbeigh_cloud.id
-  type   = "NS"
-  name   = "@"
-  value  = "ns3.digitalocean.com."
-}
-
-resource "digitalocean_record" "bruce_denbeigh_cloud" {
-  domain = data.digitalocean_domain.denbeigh_cloud.id
-  type   = "A"
-  name   = "bruce"
-  value  = "23.145.80.211"
-}
-
-resource "digitalocean_record" "bruce_tailscale_denbeigh_cloud" {
-  domain = data.digitalocean_domain.denbeigh_cloud.id
-  type   = "A"
-  name   = "bruce.tailscale"
-  value  = data.tailscale_devices.bruce.devices[0].addresses[0]
-}
-
-resource "digitalocean_record" "tailscale_denbeigh_cloud" {
+resource "cloudflare_dns_record" "tailscale_denbeigh_cloud" {
   for_each = toset(local.tailscale_aliases)
 
-  domain = data.digitalocean_domain.denbeigh_cloud.id
-  type   = "CNAME"
-  name   = each.key
-  value  = "bruce.tailscale.denbeigh.cloud."
+  zone_id = data.cloudflare_zone.denbeigh_cloud.id
+  name    = each.key
+  content = "bruce.tailscale.denbeigh.cloud."
+  type    = "CNAME"
+  ttl     = 3600
+  proxied = false
 }
